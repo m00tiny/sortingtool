989282d52856821bb328cb3db5d0bc18cf9b8419
Stuart Gray <42702437+m00tiny@users.noreply.github.com>
2023-04-09 01:39:25 -0400

    Stage 2 complete

--- Sorting Tool (Kotlin)/task/src/sorting/Main.kt
+++ Sorting Tool (Kotlin)/task/src/sorting/Main.kt
@@ -2,15 +2,24 @@ package sorting
 
 import java.util.Scanner
 
-fun main() {
+fun main(args: Array<String>) {
+    if (args.isNotEmpty()) {
+        when (args[0] == "-dataType") {
+            (args[1] == "long") -> long()
+            (args[1] == "line") -> line()
+            (args[1] == "word") -> word()
+            else -> word()
+        }
+    } else word()
+}
+
+fun long() {
     val scanner = Scanner(System.`in`)
     val numbersOnly = mutableListOf<Int>()
     var greatestNumber = 0
     var greatestNumOccur = 0
-    var numbersTotal = 0
     while (scanner.hasNext()) {
         val tempVar = scanner.nextInt()
-        numbersTotal += 1
         numbersOnly.add(tempVar)
         if (tempVar > greatestNumber) {
             greatestNumber = tempVar
@@ -19,6 +28,46 @@ fun main() {
             greatestNumOccur += 1
         }
     }
-    println("Total numbers: $numbersTotal.")
-    println("The greatest number: $greatestNumber ($greatestNumOccur time(s)).")
+    println("Total numbers: ${numbersOnly.size}.")
+    println("The greatest number: $greatestNumber ($greatestNumOccur time(s), ${greatestNumOccur/numbersOnly.size}%).")
+}
+
+fun line() {
+    val scanner = Scanner(System.`in`)
+    val linesOnly = mutableListOf<String>()
+    var longestLine = ""
+    var longestLineOccur = 0
+    while (scanner.hasNext()) {
+        val tempVar = scanner.nextLine()
+        linesOnly.add(tempVar)
+        if (tempVar.length > longestLine.length) {
+            longestLine = tempVar
+            longestLineOccur = 1
+        } else if (tempVar == longestLine) {
+            longestLineOccur += 1
+        }
+    }
+    println("Total lines: ${linesOnly.size}.")
+    println("The longest line:")
+    println(longestLine)
+    println("(${longestLineOccur} time(s), ${((longestLineOccur.toDouble()/linesOnly.size.toDouble()) * 100).toInt()}%).")
+}
+
+fun word() {
+    val scanner = Scanner(System.`in`)
+    val wordsOnly = mutableListOf<String>()
+    var longestWord = ""
+    var longestWordOccur = 0
+    while (scanner.hasNext()) {
+        val tempVar = scanner.next()
+        wordsOnly.add(tempVar)
+        if (tempVar.length > longestWord.length) {
+            longestWord = tempVar
+            longestWordOccur = 1
+        } else if (tempVar == longestWord) {
+            longestWordOccur += 1
+        }
+    }
+    println("Total words: ${wordsOnly.size}.")
+    println("The longest word: $longestWord ($longestWordOccur time(s), ${((longestWordOccur.toDouble()/wordsOnly.size.toDouble()) * 100).toInt()}%).")
 }
